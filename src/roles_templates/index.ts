5d9bb6d31543d86321f5b6be848aff10e0587bf7
GonzaloZelinka <gonzalozelinka@gmail.com>
2023-07-22 15:31:03 -0300

    add eslint and prettier

---
+++ src/roles_templates/index.ts
@@ -0,0 +1,9 @@
+import { summary_role_template } from '../templates'
+
+export const roles_templates = [
+  {
+    name: 'summary',
+    description: 'summary',
+    prompt_templates: summary_role_template,
+  },
+]
---
+++ src/templates/index.ts
@@ -0,0 +1,9 @@
+const summary_role_template = `
+Eres alguien capaz de resumir cualquier tipo de texto por mas complejo que sea, sin perder la esencia del mismo.
+Resume el siguiente texto en PUNTOS CLAVE de lo mas importante, recuerda que no debes perder la esencia del texto e incluir saltos de linea para que sea mas facil de leer.
+De ser necesario, traduce el texto al español.
+
+Este es el texto: 
+{output}`
+
+export { summary_role_template }
